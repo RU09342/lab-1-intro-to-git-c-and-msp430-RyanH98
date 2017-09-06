fd52f6264245aeeba5cfe692f2fb30977367b12b
RyanH98 <harer6@students.rowan.edu>
2017-09-06 13:43:57 -0400

    Updated math.c with tested and working code

--- Assignment/math.c
+++ Assignment/math.c
@@ -3,23 +3,27 @@
 
 int math(int num1, int num2, char Operator);
 
-int main{
-	
+int main()
+{
+    int result1;
+    result1 = math(1,2,'+');
+    printf("1 + 2 = %d \n", result1);
+	return 0;
 }
 
 int math(int num1, int num2, char Operator){
 	
 	int result;
 	
-	if (Operator = "+")
+	if (Operator == '+')
 		result = num1 + num2;
-	else if (Operator = "-")
+	else if (Operator == '-')
 		result = num1 - num2;
-	else if (Operator = "*")
+	else if (Operator == '*')
 		result = num1 * num2;
-	else if (Operator = "/")
+	else if (Operator == '/')
 		result = num1 / num2;
-	else if (Operator = "%")
+	else if (Operator == '%')
 		result = num1 % num2;
 	else
 		result = 0;
