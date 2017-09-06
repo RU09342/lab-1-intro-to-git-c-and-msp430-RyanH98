cd4b803398192b590606a8af5b4e1dd615539039
RyanH98 <harer6@students.rowan.edu>
2017-09-06 12:27:34 -0400

    Added math.c and basic (untested) code. Added basic description to readme

--- Assignment/README.md
+++ Assignment/README.md
@@ -1,1 +1,6 @@
-# YOU NEED TO POPULATE THIS README!!!!!!!!
+# Lab 1 Code
+
+## This code is for the basic inner workings of a calculator
+
+math.c supports an input of two numbers and an operator. The operator can be '+' for addition, '-' for subtraction, '*' for multiplication, '/' for division, or '%' for modulus.
+The function will return the outcome of the desired operator. It will also return 0 if the operator entered is invalid.
---
+++ Assignment/math.c
@@ -0,0 +1,24 @@
+int math(int num1, int num2, char Operator);
+
+#include "math.h"
+
+int math(int num1, int num2, char Operator)
+{
+	int result;
+	
+	if (Operator = "+")
+		result = num1 + num2;
+	else if (Operator = "-")
+		result = num1 - num2;
+	else if (Operator = "*")
+		result = num1 * num2;
+	else if (Operator = "/")
+		result = num1 / num2;
+	else if (Operator = "%")
+		result = num1 % num2;
+	else
+		result = 0;
+	
+	return result;
+	
+}
