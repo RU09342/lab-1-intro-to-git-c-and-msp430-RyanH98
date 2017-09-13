4ee393781c3abd7f8890bf8ce88c779afc0cb394
RyanH98 <harer6@students.rowan.edu>
2017-09-13 11:49:47 -0400

    Updated math function with case statements instead of if statements. Added more extensive comments.

--- Assignment/math.c
+++ Assignment/math.c
@@ -4,6 +4,11 @@
 	 *  Created on: Sept 6, 2017
 	 * 	Last Edited: Sept 13, 2017
 	 *      Author: Ryan Hare
+	 *  
+	 *  The following code implements a math function. The math function takes an input of (int, int, char). The integers are the numbers
+	 *  that the operator is performed on. For example, math(2, 3, '+') would add 2 + 3. The char can be '+' for addition, '-' for subtraction, 
+	 *  '*' for multiplication, '/' for division, or '%' for modulus. If an invalid char is entered, the function returns 0.
+	 * 
 	 */
 
 #include <stdio.h>
@@ -14,16 +19,18 @@ int math(int num1, int num2, char Operator); //Initialize the math function
 int main()
 {
     int result1; //Create a test integer.
-    result1 = math(1,2,'+'); //Do a test math function with each operator and print the result.
-    printf("1 + 2 = %d \n", result1);
-	result1 = math(2,1,'-');
-	printf("2 - 1 = %d \n", result1);
-	result1 = math(2,4,'*');
-	printf("2 * 4 = %d \n", result1);
-	result1 = math(4,2,'/');
-	printf("4 / 2 = %d \n", result1);
-	result1 = math(12,3,'%');
-	printf("12 % 3 = %d \n", result1);
+	
+	//Do a test math function with each operator and print the result.
+    result1 = math(1,2,'+'); //Test addition.
+    printf("1 + 2 = %d \n", result1); //Output result of addition.
+	result1 = math(2,1,'-'); //Test subtraction.
+	printf("2 - 1 = %d \n", result1); //Output result of subtraction.
+	result1 = math(2,4,'*'); //Test multiplication.
+	printf("2 * 4 = %d \n", result1); //Output result of multiplication.
+	result1 = math(4,2,'/'); //Test division.
+	printf("4 / 2 = %d \n", result1); //Output result of division.
+	result1 = math(10,3,'%'); //Test modulus.
+	printf("10 % 3 = %d \n", result1); //Output result of modulus.
 	return 0;
 }
 
@@ -32,18 +39,25 @@ int math(int num1, int num2, char Operator){ //Math function
 	
 	int result; //Create a result integer.
 	
-	if (Operator == '+') //If the operator is plus, do addition.
-		result = num1 + num2;
-	else if (Operator == '-') //If operator is minus, do subtraction.
-		result = num1 - num2;
-	else if (Operator == '*') //If operator is asterisk, do multiplication.
-		result = num1 * num2;
-	else if (Operator == '/') //If operator is slash, do division.
-		result = num1 / num2;
-	else if (Operator == '%') //If operator is percent, do modulus.
-		result = num1 % num2;
-	else //If the operator doesn't match, return zero.
-		result = 0;
+	switch(Operator) { //Use a switch statement to determine which operation to perform.
+		case '+' : //When the operator is '+',
+			result = num1 + num2; //return the sum of num1 and num2.
+			break;
+		case '-' : //When the operator is '-',
+			result = num1 - num2; //return the difference of num1 and num2.
+			break;
+		case '/' : //When the operator is '/',
+			result = num1 / num2; //return the quotient of num1 and num2.
+			break;
+		case '*' : //When the operator is '*',
+			result = num1 * num2; //return the product of num1 and num2.
+			break;
+		case '%' : //When the operator is '%',
+			result = num1 % num2; //return the remainder of num1 divided by num2.
+			break;
+		default: //If the operator doesn't match any other forms,
+			result = 0; //return 0.
+	}
 	
 	return result; //Return the result.
 }
